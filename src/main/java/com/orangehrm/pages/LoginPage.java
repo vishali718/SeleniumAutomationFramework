326f114bb66c5f4bd0bd13a3790a31c13f48b659
Vishali <visu@MacBookAir.mynet>
2026-03-20 14:41:45 +0000

    Update LoginPage: fix hardcoded credentials and add helper methods

--- src/main/java/com/orangehrm/pages/LoginPage.java
+++ src/main/java/com/orangehrm/pages/LoginPage.java
@@ -1,5 +1,45 @@
 package com.orangehrm.pages;
 
-public class LoginPage {
+import org.openqa.selenium.By;
 
+import com.orangehrm.actiondriver.ActionDriver;
+
+public class LoginPage {
+	private ActionDriver actionDriver;
+	
+	private By usernameFeild = By.id("txtUsername");
+	private By passwordFeild = By.id("input[name='txtPassword']");
+	private By loginButton = By.id("btnLogin");
+	private By errorMessage = By.xpath("//span[@id='spanMessage']");
+	
+	public LoginPage(ActionDriver actionDriver) {
+		this.actionDriver = actionDriver;
+	}
+	
+	// method to perform login
+	public void login(String username, String password) {
+		actionDriver.enterText(usernameFeild, "admin");
+		actionDriver.enterText(passwordFeild, "admin123");
+		actionDriver.click(loginButton);
+	}
+	
+	public String getErrorMessage() {
+		return actionDriver.getText(errorMessage);
+	}
+	public void geterrorMessageText()
+	
+		{
+		String errorText = actionDriver.getText(errorMessage);
+		System.out.println("Error Message: " + errorText);
+	}
+	
+	public void verifyErrorMessage(String expectedMessage) {
+		String actualMessage = getErrorMessage();
+		if (actualMessage.equals(expectedMessage)) {
+			System.out.println("Error message verification passed.");
+		} else {
+			System.out.println("Error message verification failed. Expected: '" + expectedMessage + "', but got: '" + actualMessage + "'.");
+		}
+	}
+	
 }
